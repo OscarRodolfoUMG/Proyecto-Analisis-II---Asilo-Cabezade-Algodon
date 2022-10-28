d872fd0e2b3fb334c983a5b0d592d4e548e7e836
OscarRodolfoUMG <113587611+OscarRodolfoUMG@users.noreply.github.com>
2022-10-28 16:24:28 -0600

    Add files via upload

---
+++ Fase - Login/modelo/asilo/pagos_asilo.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="shortcut icon" href="../../img/icono.jpg" />
+	<?php include "../../estilos/s_asilo.php" ?>
+
+	<title>Asilo</title>
+
+
+</head>
+<body>
+	<?php
+		session_start();
+		//echo "Nombre de usuario: ".$_SESSION['usuario'];
+
+		if (!isset($_SESSION['usuario'])) {
+		    header('location: ../../home.php');
+		}
+	?>
+	<div class="head">
+		<h1>Asilo de Algodon</h1><a class="cerrar" href="../../controlador/cerrarsesion.php">Cerrar Sesion</a>
+	</div>
+	<div class="intername">
+		<h2>Bienvenido <?php echo $_SESSION['usuario'];?> </h2>
+	</div>
+
+	<ul class="menu">
+        <li><a class="nav_sol" 			href="reportes_asilo.php">Reportes</a></li>
+        <li><a class="nav_fichas" 		href="cuenta_asilo.php">Estado de Cuenta</a></li>
+        <li><a class="nav_empleados" 	href="pagos_asilo.php">Pagos y Mensualidades</a></li>
+    </ul>
+
+	<div class="container">
+		<h3>Pagos y Mensualidades</h3>
+		
+
+		
+
+	</div>
+
+	<div class="foot">
+		© Rodolfo Chávez
+	</div>
+
+</body>
+</html>
