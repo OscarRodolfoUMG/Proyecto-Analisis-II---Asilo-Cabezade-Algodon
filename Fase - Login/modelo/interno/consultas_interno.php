6797aebe4f1fd1c7b4d1af28385645be242491bf
OscarRodolfoUMG <113587611+OscarRodolfoUMG@users.noreply.github.com>
2022-10-28 16:28:16 -0600

    Add files via upload

---
+++ Fase - Login/modelo/interno/consultas_interno.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="shortcut icon" href="../../img/icono.jpg" />
+	<?php include "../../estilos/s_interno.php" ?>
+
+	<title>Inicio - Interno</title>
+
+
+</head>
+<body>
+	<?php
+		session_start();
+		//echo "Nombre de usuario: ".$_SESSION['usuario'];
+
+		if (!isset($_SESSION['usuario'])){
+		    header('location: ../../home.php');
+		}
+	?>
+	<div class="head">
+		<h1>Asilo de Algodon Ventana Interno</h1><a class="cerrar" href="../../controlador/cerrarsesion.php">Cerrar Sesion</a>
+	</div>
+	<div class="intername">
+		<h2>Bienvenido <?php echo $_SESSION['usuario']; ?> </h2>
+	</div>
+
+	<ul class="menu">
+        <li><a class="nav_consulta" href="consultas_interno.php">Consultas</a></li>
+        <li><a class="nav_fichas" href="fichas_interno.php">Fichas Medicas</a></li>
+        <li><a class="nav_datos" href="datos_interno.php">Datos</a></li>
+        <li><a class="nav_cuenta" href="cuenta_interno.php">Cuenta</a></li>
+    </ul>
+
+	<div class="container">
+		
+		<div class="consulta_pendiente">
+			<h4>Consulta Pendiente</h4>
+		</div>
+
+		
+		<div class="ultimo_diagnostico">
+			<h4>Ultimo Diagnostico</h4>
+		</div>
+
+	</div>
+
+	<div class="foot">
+		© Rodolfo Chávez
+	</div>
+
+</body>
+</html>
